020cc0104f0f2ab6e142f4542b3fd726d4e9c2be
Ruiqiang Hao <Ruiqiang.Hao@windriver.com>
2021-11-21 13:12:53 +0800

    ctrl: Add dynamic domain name resolution
    Signed-off-by: Ruiqiang Hao <Ruiqiang.Hao@windriver.com>

--- relay_ctrl.c
+++ relay_ctrl.c
@@ -11,7 +11,7 @@
 #include "iniparser.h"
 
 #define BUF_SIZE 500
-#define SOCK_PORT 8090
+#define SOCK_PORT "8090"
 
 #define ON 0
 #define OFF 1
@@ -31,6 +31,8 @@ int main(int argc, char *argv[])
     char buf[BUF_SIZE];
     int channel, state;
     dictionary *ini;
+    struct addrinfo hints;
+    struct addrinfo *result, *rp;
 
     if (argc < 3) {
 	usage(argv[0]);
@@ -72,6 +74,44 @@ int main(int argc, char *argv[])
     const char* server = iniparser_getstring(ini, "RPC:server", "NULL");
     printf("server = [%s]\n", server);
 
+    memset(&hints, 0, sizeof(struct addrinfo));
+    hints.ai_family = AF_UNSPEC;    /* Allow IPv4 or IPv6 */
+    hints.ai_socktype = SOCK_STREAM; /* Stream socket */
+    hints.ai_flags = 0;
+    hints.ai_protocol = 0;          /* Any protocol */
+
+    s = getaddrinfo(server, SOCK_PORT, &hints, &result);
+    if (s != 0) {
+	fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(s));
+	exit(EXIT_FAILURE);
+    }
+
+    /* getaddrinfo() returns a list of address structures.
+       Try each address until we successfully connect(2).
+       If socket(2) (or connect(2)) fails, we (close the socket
+       and) try the next address. */
+
+    for (rp = result; rp != NULL; rp = rp->ai_next) {
+	sfd = socket(rp->ai_family, rp->ai_socktype,
+		rp->ai_protocol);
+	if (sfd == -1)
+	    continue;
+
+	if (connect(sfd, rp->ai_addr, rp->ai_addrlen) != -1)
+	    break;                  /* Success */
+
+	close(sfd);
+    }
+
+    if (rp == NULL) {               /* No address succeeded */
+	fprintf(stderr, "Could not connect\n");
+	exit(EXIT_FAILURE);
+    }
+
+    freeaddrinfo(result);           /* No longer needed */
+
+
+#if 0
     sfd = socket(AF_INET, SOCK_STREAM, 0);
 
     dest.sin_family = AF_INET;
@@ -79,6 +119,7 @@ int main(int argc, char *argv[])
     dest.sin_addr.s_addr=inet_addr("128.224.163.137"); //128.224.162.151");
 
     connect(sfd, (struct sockaddr *)&dest, sizeof(struct sockaddr));
+#endif
 
     buf[0] = 0x55;
     buf[1] = channel-1;
@@ -89,7 +130,7 @@ int main(int argc, char *argv[])
 	fprintf(stderr, "partial/failed write\n");
 	exit(EXIT_FAILURE);
     }
-#if 1
+#if 0
     nread = read(sfd, buf, BUF_SIZE);
     if (nread == -1) {
 	perror("read");
